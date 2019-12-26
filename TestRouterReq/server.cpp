b02e57136b856371453608b4868672f74d3ff443
cr <eternal_mr@qq.com>
2019-12-26 15:02:58 +0800

    Test ReqRep socket

--- TestRouterReq/server.cpp
+++ TestRouterReq/server.cpp
@@ -4,36 +4,14 @@
 
 int main() {
 	zmq::context_t context(1);
-	zmq::socket_t broker(context, ZMQ_ROUTER);
+	zmq::socket_t server(context, ZMQ_REP);
 
-#if (defined(WIN32))
-	broker.bind("tcp://*:5671"); // "ipc" doesn't yet work on windows.
-#else
-	broker.bind("ipc://routing.ipc");
-#endif
+	//  We use a string identity for ease here
+	server.connect("tcp://localhost:5671");
 
-	const int NBR_WORKERS = 2;
-
-	//  Run for five seconds and then tell workers to end
-	int64_t end_time = s_clock() + 5000;
-	int workers_fired = 0;
-	while (1) {
-		//  Next message gives us least recently used worker
-		std::string identity = s_recv(broker);
-		s_recv(broker);     //  Envelope delimiter
-		s_recv(broker);     //  Response from worker       
-
-		s_sendmore(broker, identity);
-		s_sendmore(broker, "");
-		//  Encourage workers until it's time to fire them
-		if (s_clock() < end_time)
-			s_send(broker, "Work harder");
-		else {
-			s_send(broker, "Fired!");
-			if (++workers_fired == NBR_WORKERS)
-				break;
-		}
-	}
+	std::string reply = s_recv(server);
+	std::cout << "Receive request: " << reply << std::endl;
+	s_send(server, "do your work");
 
 	system("pause");
 
--- requester/client.cpp
+++ requester/client.cpp
@@ -4,36 +4,14 @@
 
 int main() {
 	zmq::context_t context(1);
-	zmq::socket_t worker(context, ZMQ_REQ);
+	zmq::socket_t client(context, ZMQ_REQ);
 
 	//  We use a string identity for ease here
-#if (defined (WIN32))
-	s_set_id(worker, within(0x10000));
-	worker.connect("tcp://localhost:5671"); // "ipc" doesn't yet work on windows.
-#else
-	s_set_id(worker);
-	worker.connect("ipc://routing.ipc");
-#endif
+	client.connect("tcp://localhost:5671");
 
-	int total = 0;
-	while (1) {
-		//  Tell the broker we're ready for work
-		s_send(worker, "Hi Boss");
-		
-
-		//  Get workload from broker, until finished
-		std::string workload = s_recv(worker);
-		
-		if ("Fired!" == workload) {
-			std::cout << "Processed: " << total << " tasks" << std::endl;
-			break;
-		}
-		total++;
-
-		//  Do some random work
-		s_sleep(within(500) + 1);
-		std::cout << "Processing: Task " << total << std::endl;
-	}
+	s_send(client, "ready");
+	std::string reply = s_recv(client);
+	std::cout << "Receive reply: " << reply << std::endl;
 
 	system("pause");
 
